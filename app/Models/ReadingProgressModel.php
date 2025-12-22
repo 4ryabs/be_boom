d8535347e7885d4dacc6db3625c1b07bafc3f5b7
jhoajaa <jhosyoajaa@gmail.com>
2025-12-23 01:14:31 +0700

    make readingprogress (entities and models)

---
+++ app/Models/ReadingProgressModel.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ReadingProgressModel extends Model
+{
+    protected $table            = 'readingprogresses';
+    protected $primaryKey       = 'progress_id';
+    protected $useAutoIncrement = false;
+    protected $returnType       = 'App\Entities\ReadingProgress';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['progress_id', 'book_id', 'total_pages', 'current_page', 'status_baca'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = ['generateUUID'];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    protected function generateUUID(array $data)
+    {
+        if (! isset($data['data'][$this->primaryKey])) {
+            $data['data'][$this->primaryKey] = $this->getUuid();
+        }
+        return $data;
+    }
+
+    private function getUuid()
+    {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
+}
